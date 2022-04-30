46ee11d8dc78384a21af553797ad66a2ec87bdd9
Alexandr Pavlyuk <sasha.pavlyuk.1996@inbox.ru>
2022-04-30 23:39:49 +0700

    added signUp controller

--- src/main/java/ru/alexpvl/guestbookfx/client/controllers/SignInController.java
+++ src/main/java/ru/alexpvl/guestbookfx/client/controllers/SignInController.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class SingInController {
+public class SignInController {
     String login;
     String password;
 
